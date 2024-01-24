a8d97c43c21678c23a63fa5c115d8f3e863f59c7
Efim Zabarsky <EZ>
2024-01-24 09:56:07 +0200

    rfct

--- WebApplicationBakery/WebApplicationBakery/Controllers/BakeryControllerGraphQl.cs
+++ WebApplicationBakery/WebApplicationBakery/Controllers/BakeryControllerGraphQl.cs
@@ -1,59 +1,58 @@
-﻿using GraphQL;
-using GraphQL.AspNet.Attributes;
+﻿using GraphQL.AspNet.Attributes;
 using GraphQL.AspNet.Controllers;
 
 namespace WebApplicationBakery.Controllers
 {
 	public class BakeryControllerGraphQl : GraphController
 	{
-		List<Donut> donuts;
-		List<Cake> cakes;
+		List<Donut> _donuts;
+		List<Cake> _cakes;
 
 		public BakeryControllerGraphQl()
 		{
-			donuts = new DonutsCollection().Donuts;
-			cakes = new CakeCollection().Cakes;
+			_donuts = new DonutsCollection().Donuts;
+			_cakes = new CakeCollection().Cakes;
 		}
 
 		//A Basic Controller
 		[QueryRoot("firstDonut")]
-		public Donut FirstDonut() => donuts[0];
+		public Donut firstDonut() => _donuts[0];
 
 		[QueryRoot("lastDonut")]
-		public Donut LastDonut() => donuts[donuts.Count - 1];
+		public Donut lastDonut() => _donuts[_donuts.Count - 1];
 
-		[QueryRoot("allDonuts")]
-		public List<Donut> AllDonuts() => donuts;
+		[QueryRoot("donuts")]
+		public List<Donut> donuts() => _donuts;
 
-		[QueryRoot("allCakes")]
-		public List<Cake> AllCakes() => cakes;
+		[QueryRoot("cakes")]
+		public List<Cake> cakes() => _cakes;
 
-		[QueryRoot("DonutByID")]
-		public Donut DonutByID(int id) => donuts.Where(x => x.Id == id).FirstOrDefault();
+		[QueryRoot("donutByID")]
+		public Donut donutByID(int id) => _donuts.Where(x => x.Id == id).FirstOrDefault();
 
 		//Using an Interface
 		[QueryRoot(typeof(Cake), typeof(Donut))]
-		public ISweet GetSweet(string sweetType)
+		public ISweet? getSweet(string sweetType)
 		{
 			switch (sweetType)
 			{
 				case "Donut":
-					return donuts[0];
+					return _donuts[0];
 				case "Cake":
-					return cakes[0];
+					return _cakes[0];
 				default: return null;
 			}
 		}
 
 		[QueryRoot(typeof(ISweet))]
-		public ISweet GetSweetEnum(SweetType sweetType)
+		public ISweet? getSweetEnum(SweetType sweetType)
 		{
 			switch (sweetType)
 			{
 				case SweetType.Donut:
-					return donuts[0];
+					return _donuts[0];
 				case SweetType.Cake:
-					return cakes[0];
+					return _cakes[0];
 				default: return null;
 			}
 		}
